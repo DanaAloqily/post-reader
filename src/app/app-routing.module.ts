0348da5187607dde50ef67ec6add85a8c922d939
DanaAloqily <danaaloqily@hotmail.com>
2023-01-19 16:07:27 +0300

    Navbar, Nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { RecipeComponent } from './recipe/recipe.component';
+import { PostsComponent } from './posts/posts.component';
+const routes: Routes = [
+  {path:'',component: PostsComponent}, // root path
+  {path:'recipe', component: RecipeComponent} //
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
 import { RecipeComponent } from './recipe/recipe.component';
 import { PostItemComponent } from './post-item/post-item.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostsComponent,
     RecipeComponent,
-    PostItemComponent
+    PostItemComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
